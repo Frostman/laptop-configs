1f8f898e3e128b5d03ca715bfb5ac849a1894b75
Sergey Lukjanov <me@slukjanov.name>
2017-01-19 23:47:19 -0800

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -337,6 +337,15 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
+####
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
